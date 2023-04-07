296ce79d73041c32caf2cfb2e791fd7fd39d743f
aayushsenapati <92628133+aayushsenapati@users.noreply.github.com>
2023-04-07 09:06:11 +0530

    loll

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 ﻿var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
-var io = require('socket.io').listen(server);
+var io = require('socket.io')(server);
 
 app.use(express.static(__dirname + '/'));
 
