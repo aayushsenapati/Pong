af62c1b156692b5ceeebc2916750c290e93e09b8
AayushNagarr <92391624+AayushNagarr@users.noreply.github.com>
2023-04-08 02:22:11 +0530

    awf

--- public/app.js
+++ public/app.js
@@ -2,12 +2,14 @@ let canvas = document.getElementById("gameCanvas");
 let engine = new BABYLON.Engine(canvas, true);
 
 let createScene = function () {
+    
     let physicsPlugin = new BABYLON.CannonJSPlugin();
     let scene = new BABYLON.Scene(engine);
     scene.enablePhysics(new BABYLON.Vector3(0, 0, 0), physicsPlugin);
     scene.clearColor = new BABYLON.Color4(0, 0, 0, 1);
 
     let material = new BABYLON.StandardMaterial("material", scene);
+    let material1 = new BABYLON.StandardMaterial("material", scene);
     material.diffuseColor = new BABYLON.Color3(255, 1, 1);
 
     // Light
@@ -21,24 +23,24 @@ let createScene = function () {
 
     // Create paddles and walls
 
-    let wall1 = BABYLON.MeshBuilder.CreateBox("wall1", { height: 0.5, width: 10, depth: 0.3 }, scene);  //wall farthest from camera
+    let wall1 = BABYLON.MeshBuilder.CreateBox("wall1", { height: 1, width: 10, depth: 0.3 }, scene);  //wall farthest from camera
     wall1.material = material;
     wall1.position.x = 4.5;
     wall1.position.z = 0;
     wall1.rotation.y = Math.PI / 2;
     
-    let wall2 = BABYLON.MeshBuilder.CreateBox("wall2", { height: 0.5, width: 10, depth: 0.3 }, scene); //wall closest to camera
+    let wall2 = BABYLON.MeshBuilder.CreateBox("wall2", { height: 1, width: 10, depth: 0.3 }, scene); //wall closest to camera
     wall2.material = material;
     wall2.position.x = -4.75;
     wall2.position.z = 0;
     wall2.rotation.y = Math.PI / 2;
     
-    let wall3 = BABYLON.MeshBuilder.CreateBox("wall3", { height: 0.5, width: 10, depth: 0.3 }, scene); //wall to the right of camera
+    let wall3 = BABYLON.MeshBuilder.CreateBox("wall3", { height: 1, width: 10, depth: 0.3 }, scene); //wall to the right of camera
     wall3.material = material;
     wall3.position.x = 0;
     wall3.position.z = -4.8;
 
-    let wall4 = BABYLON.MeshBuilder.CreateBox("wall4", { height: 0.5, width: 10, depth: 0.3 }, scene); //wall to the left of camera
+    let wall4 = BABYLON.MeshBuilder.CreateBox("wall4", { height: 1, width: 10, depth: 0.3 }, scene); //wall to the left of camera
     wall4.material = material;
     wall4.position.x = 0;
     wall4.position.z = 4.8;
@@ -48,6 +50,7 @@ let createScene = function () {
     let rightPaddle = BABYLON.MeshBuilder.CreateBox("rightPaddle", { height: 0.5, width: 0.5, depth: 2 }, scene);
     leftPaddle.position.x = -4;
     rightPaddle.position.x = 4;
+    
     leftPaddle.position.y = rightPaddle.position.y = 0.3;
 
     // Create ball
@@ -67,10 +70,14 @@ let createScene = function () {
     
     // Set physics properties
     scene.enablePhysics(new BABYLON.Vector3(0, -9.81, 0), physicsPlugin);
-    plane.physicsImpostor = new BABYLON.PhysicsImpostor(plane, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 0, restitution: 0.9 }, scene);
-    leftPaddle.physicsImpostor = new BABYLON.PhysicsImpostor(leftPaddle, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 1000000000,  restitution: 0.1 }, scene);
-    rightPaddle.physicsImpostor = new BABYLON.PhysicsImpostor(rightPaddle, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 1000000000, restitution: 0.1 }, scene);
-    ball.physicsImpostor = new BABYLON.PhysicsImpostor(ball, BABYLON.PhysicsImpostor.SphereImpostor, { mass: 1, restitution: 0.9 }, scene);
+    plane.physicsImpostor = new BABYLON.PhysicsImpostor(plane, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 0, restitution: 1 }, scene);
+    leftPaddle.physicsImpostor = new BABYLON.PhysicsImpostor(leftPaddle, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 1000000000,  restitution: 1 }, scene);
+    rightPaddle.physicsImpostor = new BABYLON.PhysicsImpostor(rightPaddle, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 1000000000, restitution: 1 }, scene);
+    wall4.physicsImpostor = new BABYLON.PhysicsImpostor(wall4, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 0, restitution: 1  }, scene);
+    wall1.physicsImpostor = new BABYLON.PhysicsImpostor(wall1, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 0, restitution: 1  }, scene);
+    wall2.physicsImpostor = new BABYLON.PhysicsImpostor(wall2, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 0, restitution: 1  }, scene);
+    wall3.physicsImpostor = new BABYLON.PhysicsImpostor(wall3, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 0, restitution: 1  }, scene);
+    ball.physicsImpostor = new BABYLON.PhysicsImpostor(ball, BABYLON.PhysicsImpostor.SphereImpostor, { mass: 1, restitution: 1  }, scene);
     
   
     
@@ -79,13 +86,13 @@ let createScene = function () {
         if (event.code === 'KeyA' && leftPaddle.position.z < 4) {
             leftPaddle.position.z += 0.5;
             socket.emit('paddleMoved', { player: 'left', position: leftPaddle.position.z });
-        } else if (event.code === 'KeyZ' && leftPaddle.position.z > -4) {
+        } else if (event.code === 'KeyD' && leftPaddle.position.z > -4) {
             leftPaddle.position.z -= 0.5;
             socket.emit('paddleMoved', { player: 'left', position: leftPaddle.position.z });
         }
     });
     
-    ball.physicsImpostor.setLinearVelocity(new BABYLON.Vector3(-4, 0, 0));
+    ball.physicsImpostor.setLinearVelocity(new BABYLON.Vector3(5, 0, Math.random()*10));
     // Create variables for ball movement
     // Create variables for ball movement
     let xDirection = Math.random() < 0.5 ? -1 : 1;
@@ -97,28 +104,32 @@ let createScene = function () {
         // ball.position.z += zDirection * 0.04;
         
         //wall collision handlers 
-        
+        let vel = ball.physicsImpostor.getLinearVelocity();
+        ball.physicsImpostor.setLinearVelocity(new BABYLON.Vector3(vel.x, -10, vel.z));
 
-          ball.physicsImpostor.registerOnPhysicsCollide(leftPaddle.physicsImpostor, (main, collided, contact) => {
-            console.log("Collision detected!");
-           console.log(contact);
+          ball.physicsImpostor.registerOnPhysicsCollide(leftPaddle.physicsImpostor, (main, collided) => {
+           
+           
             
           });
           ball.physicsImpostor.registerOnPhysicsCollide(wall1.physicsImpostor, (main, collided) => {
-            ball.physicsImpostor.setLinearVelocity(new BABYLON.Vector3(4, 0, 0));
-            console.log(ball.physicsImpostor.getLinearVelocity());
+            
+            
+            material1.diffuseColor = new BABYLON.Color3(Math.random(), Math.random(), Math.random());
+            ball.material = material1;
           });
           ball.physicsImpostor.registerOnPhysicsCollide(wall2.physicsImpostor, (main, collided) => {
-            ball.physicsImpostor.setLinearVelocity(new BABYLON.Vector3(4, 0, 0));
-            console.log(ball.physicsImpostor.getLinearVelocity());
+            material1.diffuseColor = new BABYLON.Color3(Math.random(), Math.random(), Math.random());
+            ball.material = material1;
+          
           });
           ball.physicsImpostor.registerOnPhysicsCollide(wall3.physicsImpostor, (main, collided) => {
-            ball.physicsImpostor.setLinearVelocity(new BABYLON.Vector3(4, 0, 0));
-            console.log(ball.physicsImpostor.getLinearVelocity());
+            material1.diffuseColor = new BABYLON.Color3(Math.random(), Math.random(), Math.random());
+            ball.material = material1;
           });
           ball.physicsImpostor.registerOnPhysicsCollide(wall4.physicsImpostor, (main, collided) => {
-            ball.physicsImpostor.setLinearVelocity(new BABYLON.Vector3(4, 0, 0));
-            console.log(ball.physicsImpostor.getLinearVelocity());
+            material1.diffuseColor = new BABYLON.Color3(Math.random(), Math.random(), Math.random());
+            ball.material = material1;
           });
 
         // Check for collisions with the paddles
@@ -128,20 +139,7 @@ let createScene = function () {
             xDirection = -1;
         }
 
-        // Check for collisions with the walls
-        if (ball.intersectsMesh(wall2, false)) {
-            xDirection = 1;
-        }
-        if (ball.intersectsMesh(wall1, false)) {
-            xDirection = -1;
-        }
-        
-        if (ball.intersectsMesh(wall3, false)) {
-            zDirection = 1;
-        }
-        if (ball.intersectsMesh(wall4, false)) {
-            zDirection = -1;
-        }
+      
     };
 
     // Start the game loop
